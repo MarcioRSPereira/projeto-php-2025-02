9aa1381c249cfbc7591fca1c3dcdb04d5aac366e
fluizneto <francisco.neto006@cs.unipe.edu.br>
2025-11-19 14:26:51 -0300

    Add clientesAlterar.php for client update functionality

---
+++ admin/clientesAlterar.php
@@ -0,0 +1,34 @@
+<?php
+
+require_once "config.inc.php";
+
+if($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $cliente = $_POST["cliente"];
+    $loja = $_POST["loja"];
+    $cnpj = $_POST["cnpj"];
+    $endereco = $_POST["endereco"];
+    $numero = $_POST["numero"];
+    $email = $_POST["email"];
+    $id = $_POST["id"];
+
+    $sql = "UPDATE clientes SET 
+            cliente = '$cliente',
+            loja = '$loja',
+            cnpj = '$cnpj',
+            endereco = '$endereco',
+            numero = '$numero',
+            email='$email'
+            WHERE id = '$id'";
+
+    $executa = mysqli_query($conexao, $sql);
+    if($executa) {
+        echo "<h2>Alteração realizada com sucesso.</h2>";
+        echo "<a href='?pg=clienteslista'>Voltar</a>";
+    }else{
+        echo "<h2>Erro ao alterar cadastro.</h2>";
+        echo "<a href='?pg=clienteslista'>Voltar</a>";
+    }
+}else{
+    echo "<h2>Acesso negado.</h2>";
+    echo "<a href='?pg=clienteslista'>Voltar</a>";
+}
