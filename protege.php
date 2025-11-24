daf28aa2f5e99ea10b15f3769838d6d049cf6367
fluizneto <francisco.neto006@cs.unipe.edu.br>
2025-11-24 12:39:06 -0300

    Add files via upload

---
+++ protege.php
@@ -0,0 +1,11 @@
+<?php
+  if(!function_exists("protege")){
+    function protege(){
+        if(!isset($_SESSION))
+            session_start();
+        if(!isset($_SESSION['usuario'])||!is_numeric($_SESSION['usuario'])){
+            header("Location: login.php");
+        }
+    }
+  }
+?>
