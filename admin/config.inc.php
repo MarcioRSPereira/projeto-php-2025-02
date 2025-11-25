4321d4ed9e9e5ce9ced517e6ba7d457025eceb88
fluizneto <francisco.neto006@cs.unipe.edu.br>
2025-11-25 13:38:11 -0300

    Update database name in config file

--- admin/config.inc.php
+++ admin/config.inc.php
@@ -2,8 +2,9 @@
     //1ª etapa - conectando com servidor
     $conexao = mysqli_connect("localhost","root","");
     //2ª etapa - selecionando banco de dados
-    $db = mysqli_select_db($conexao,"projeto1");
+    $db = mysqli_select_db($conexao,"projetofinal");
 
     if(!$conexao){
         echo "<h2>Erro ao conectar o banco de dados</h2>";
     }
+
