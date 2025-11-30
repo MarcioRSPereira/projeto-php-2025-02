95c647b505c52e19e42b4c1c0e2fcd827afad79c
fluizneto <francisco.neto006@cs.unipe.edu.br>
2025-11-30 13:49:52 -0300

    Fix SQL syntax for deleting client record

--- admin/clientesExcluir.php
+++ admin/clientesExcluir.php
@@ -3,7 +3,7 @@
     require_once "config.inc.php";
     $id = $_GET['id'];
 
-    $sql = "DELETE FROM clientes WHERE id = "$id";
+    $sql = "DELETE FROM clientes WHERE id = $id";
 
     $resultado = mysqli_query($conexao, $sql);
     if($resultado){
