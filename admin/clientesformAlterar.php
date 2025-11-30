19dac0e003baf408c98185808e34fb48d5c9ebc9
fluizneto <francisco.neto006@cs.unipe.edu.br>
2025-11-30 13:44:34 -0300

    Refactor cliente form handling in clientesformAlterar.php

--- admin/clientesformAlterar.php
+++ admin/clientesformAlterar.php
@@ -1,50 +1,31 @@
 <?php
-    // Inclui a conexão com o BD.
-    require_once "config.inc.php";
-    
-    // Pega o ID do cliente da URL.
-    $id = $_GET['id'];
-    
-    // Consulta o BD para buscar os dados do cliente por ID.
-    $sql = "SELECT * FROM clientes WHERE id = '$id'";
-    
-   //consulta
-    $resultado = mysqli_query($conexao, $sql);
-    
-    // Verifica se o cliente foi encontrado.
-    if(mysqli_num_rows($resultado) > 0){
-        
-        // Carrega os dados do cliente no array $cliente.
-        $cliente = mysqli_fetch_array($resultado);
+    require_once "config.inc.php";
+    $id = $_GET['id'];
+    $sql = "SELECT * FROM clientes WHERE id = '$id'";
+    $resultado = mysqli_query($conexao, $sql);
+    if(mysqli_num_rows($resultado) > 0){
+        $cliente = mysqli_fetch_array($resultado);
+
 
 ?>
 
 <h2>Cadastro de Clientes</h2>
-
 <form action="?pg=clientesAlterar" method="post">
+    <input type="hidden" name="id" value="<?=$cliente['id']?>">
+    <label>Cliente:</label>
+    <input type="text" name="cliente" value="<?=$cliente['cliente']?>"><br>
+    <label>Loja:</label>
+    <input type="text" name="loja" value="<?=$cliente['loja']?>"><br>
+    <label>CNPJ:</label>
+    <input type="number" name="cnpj" value="<?=$cliente['cnpj']?>"><br>
+    <label>Endereço:</label>
+    <input type="text" name="endereco" value="<?=$cliente['endereco']?>"><br>
+    <label>Número:</label>
+    <input type="number" name="numero" value="<?=$cliente['numero']?>"><br>
+    <label>Email:</label>
+    <input type="email" name="email" value="<?=$cliente['email']?>"><br>
 
-        <input type="hidden" name="id" value="<?=$cliente['id']?>">
-    
-        <label>Cliente:</label>
-    <input type="text" name="cliente" value="<?=$cliente['cliente']?>"><br>
-    <label>Loja:</label>
-    <input type="text" name="loja" value="<?=$cliente['loja']?>"><br>
-    <label>CNPJ:</label>
-    <input type="number" name="cnpj" value="<?=$cliente['cnpj']?>"><br>
-    <label>Endereço:</label>
-    <input type="text" name="endereco" value="<?=$cliente['endereco']?>"><br>
-    <label>Número:</label>
-    <input type="number" name="numero" value="<?=$cliente['numero']?>"><br>
-    <label>Email:</label>
-    <input type="email" name="email" value="<?=$cliente['email']?>"><br>
-
-        <input type="submit" value="Alterar cliente">
+    <input type="submit" value="Alterar cliente">
 </form>
 
 <?php
-    }else{
-        // Mensagem de erro se o cliente não for encontrado.
-        echo "<h2>Nenhum cliente cadastrado!</h2>";
-    }
-?>
-
