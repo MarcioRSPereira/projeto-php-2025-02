ecdb3c131ee66ffe70c0f60013f213af3edddb74
fluizneto <francisco.neto006@cs.unipe.edu.br>
2025-11-30 13:43:27 -0300

    Update admin panel navigation and content handling

--- admin/admin.php
+++ admin/admin.php
@@ -1,39 +1,43 @@
-<?php
 
-echo "<h1>Painel Administrativo</h1>";
+       <?php
+    echo "<h1>Painel Administrativo</h1>";
+
+    
 ?>
 <nav class="navbar navbar-expand-sm bg-primary navbar-dark">
-    <div class="container-fluid">
-        <ul class="navbar-nav">
-                        <li class="nav-item">
-                <a class="nav-link" href="?pg=clientesLista"> listar clientes</a>
-            </li>
-            </li>
-        </ul>
-    </div>
+    <div class="container-fluid">
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" href="?pg=paginas"> Gestão de Páginas</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="?pg=noticias"> Noticias</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link"  href="?pg=clienteslista"> Clientes</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="?pg=contato"> Dados de Contato</a>
+            </li>
+        </ul>
+    </div>
 </nav>
 
 <?php
 
-
-    
-    // Verifica se a URL está vazia, ou seja, se não há nenhum parâmetro GET após o '?'.
-    if(empty($_SERVER["QUERY_STRING"])){
-        
-        // Define 'clientes' como o nome do arquivo padrão a ser carregado.
-        $var = "clientes";
-        
-        // Inclui o arquivo de conteúdo padrão (clientes.php).
-        include_once "$var.php";
-    }else{
-        
-        // Obtém o valor'pg' da URL (ex: se for `?pg=clientesLista`, $pg será "clientesLista").
-        $pg = $_GET['pg'];
-        
-        // Inclui o arquivo cujo nome corresponde ao valor de 'pg' (ex: clientesLista.php)
-        include_once "$pg.php";
+    // área de conteúdo
+    if(empty($_SERVER["QUERY_STRING"])){
+        $var = "clientes";
+        include_once "$var.php";
+    }else{
+        $pg = $_GET['pg'];
+        include_once "$pg.php";
     }
 ?>
+
+
+
+
 
 
 
